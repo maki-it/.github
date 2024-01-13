ee2a83136953dd6faad7b8822870c8868ede75e9
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-01-13 16:29:03 +0100

    Fix paths

--- renovate/config.js
+++ renovate/config.js
@@ -3,9 +3,9 @@ module.exports = {
     onboardingConfig: {
         $schema: 'https://docs.renovatebot.com/renovate-schema.json',
         extends: [
-            "github>maki-it/.github/renovate:base",
-            "github>maki-it/.github/renovate:default-update-type-rules",
-            "github>maki-it/.github/renovate:default-schedules"
+            "github>maki-it/.github//renovate:base",
+            "github>maki-it/.github//renovate:default-update-type-rules",
+            "github>maki-it/.github//renovate:default-schedules"
         ],
     },
 }
