942338d5587ffb43a43e0a5f7d6239496bbdc7a7
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-01-13 14:53:45 +0000

    Add renovate config

---
+++ renovate/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    autodiscover: true,
+    onboardingConfig: {
+        $schema: 'https://docs.renovatebot.com/renovate-schema.json',
+        extends: [
+            "github>maki-it/.github/renovate:base",
+            "github>maki-it/.github/renovate:default-update-type-rules",
+            "github>maki-it/.github/renovate:default-schedules"
+        ],
+    },
+}
